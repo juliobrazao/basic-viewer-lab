1437bc4680c16e7ffdf3f3ee5e0840430f2c3b6e
Julio Brazao <julio@brazao.dev>
2025-06-20 14:59:33 -0400

    Tanstack query assets

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-root.render(<App />);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
